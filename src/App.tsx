bdd64ddd96feef846d7cd2c3fa4d475ac8ff5fd5
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-09-16 12:17:06 -0300

    Created Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import Modal from 'react-modal'
+
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
 import { NewTransactionModal } from './components/NewTransactionModal'
+
 import { GlobalStyle } from './styles/global'
+
+import { TransactionsProvider } from './TransactionsContext'
 
 Modal.setAppElement('#root')
 
@@ -19,7 +23,7 @@ export function App() {
     setIsNewTransactionModalOpen(false)
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -27,6 +31,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   )
 }
