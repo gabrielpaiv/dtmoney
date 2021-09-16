839c3a20b0e3db1bd1145b742ec661d4f0fa7bdf
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-09-15 21:02:38 -0300

    Created Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import Logo from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+function Header() {
+  return (
+    <Container>
+      <Content>
+        <img src={Logo} alt="dt money" />
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  )
+}
+
+export { Header }
